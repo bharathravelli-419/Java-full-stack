abb2be6cd4bf2b73fef802ace8ea4b65e37f9a84
Bharath Ravelli <bharathravelli419@gmail.com>
2023-07-31 12:05:05 +0530

    String initialization & some imp. methods

--- Main.java
+++ Main.java
@@ -3,55 +3,68 @@ public class Main{
     public static void main(String[] args) {
 
        Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt(); // for n= 5 
-       
-       for(int i=0;i<(n/2 +1);i++){
-        for(int j =i;j<n/2;j++){
-            System.out.print(" ");
-        }
-        System.out.print("*");
-        for(int j =0;j<(2*(i-1) +1);j++){
-            
-            System.out.print(" ");
-        }
-        if(i !=0)
-        System.out.print("*");
-
-        System.out.println();
-
-       }
-
-       //for Lower Triangle
-
-       for(int i=0;i<n/2;i++){
-        for(int j =0;j<(i+1);j++){
-             System.out.print(" ");
-        }
-        System.out.print("*");
-        
-        for(int j =0;j< (2*(n/2-i-1) -1);j++){
-            System.out.print(" ");
-        }
-        if(i != n/2 -1)
-        System.out.print("*");
-
-        System.out.println();
-       
-         
-       }
-       //output :
-       /*
-  *
- * *
-*   *
- * *
-  *
-
-Above one is a hollow diamond
-
-        */
+       //String Initialization types
 
 
-       sc.close();
+       //assigning a String Literal 
+       String s1 = "Bharath Ravelli";
+
+       //Using String class
+       String s2 = new String("Tejaswi Ravelli");
+
+      //using character array
+      char[] arr= {'V','i','j','a','y','a'};
+      String s3 = new String(arr);
+
+      //Using a byte array 
+      String s4 = new String(new byte[]{65,66,67,68});
+      System.out.println(s1+"\n"+s2+"\n"+s3+"\n"+s4);
+      System.out.println();
+      System.out.println("Some important methods on Strings :");
+
+      System.out.println(
+        "int length():"+ s1.length()+"\n"+
+        "String toLowerCase():"+ s1.toLowerCase()+"\n"+
+        "String toUpperCase():"+s1.toUpperCase()+"\n"+
+        "String trim():"+"  spaces ".trim()+"\n"+
+        "String substring(int begin):"+ s1.substring(2)+"\n"+
+        "String substring(int begin,int end):"+s1.substring(2,5)+"\n"+
+        "String replace(char old):"+s1.replace('a','r')+"\n"+
+        "char charAt(int ind):"+s1.charAt(0)+"\n"+
+        "boolean startsWith(String s,int ind):"+s1.startsWith("R", 8)+"\n"+
+        "boolean endsWith(String s,int ind):"+s1.endsWith("lli")+"\n"+
+        "int indexOf(String s):"+s1.indexOf("Ravelli")+"\n"+
+        //"int lastIndexOf(String s)"
+        "boolean equals(String s):"+ s1.equals("Bharath Ravelli")+"\n"+
+        "boolean equalsIgnoreCase(String s):"+s1.equals("Bharathravelli")+"\n"+
+        "int compareTo(String s):"+s1.compareTo(s2)+"\n"+
+        "boolean contains(String str)"+s1.contains("Ravelli")       
+      );
     }
 }
+/*
+output:
+
+Bharath Ravelli
+Tejaswi Ravelli
+Vijaya
+ABCD
+
+Some important methods on Strings :
+int length():15
+String toLowerCase():bharath ravelli
+String toUpperCase():BHARATH RAVELLI
+String trim():spaces
+String substring(int begin):arath Ravelli
+String substring(int begin,int end):ara
+String replace(char old):Bhrrrth Rrvelli
+char charAt(int ind):B
+boolean startsWith(String s,int ind):true
+boolean endsWith(String s,int ind):true
+int indexOf(String s):8
+boolean equals(String s):true
+boolean equalsIgnoreCase(String s):false
+int compareTo(String s):-18
+boolean contains(String str)true
+
+ */
