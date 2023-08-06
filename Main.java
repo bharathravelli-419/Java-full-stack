d67f23647a3b1e193173179ac59818ccbda318d9
Bharath Ravelli <bharathravelli419@gmail.com>
2023-08-06 16:04:15 +0530

    Inheritence and Interfaces and Dynamic method dispatch

--- Main.java
+++ Main.java
@@ -1,70 +1,43 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args) {
+import java.lang.String;
 
-       Scanner sc = new Scanner(System.in);
-       //String Initialization types
+interface IDemo{
+    final static int x=10;;
+   
+    public static int meth1(){
+        System.out.println(meth2());
+        return Integer.MAX_VALUE;
+    }
+   private static int  meth2(){
+    return -20;
+   }
+  default String meth3(){
+    return ("Default method() called");
+   }
 
+}
+class A implements IDemo{
 
-       //assigning a String Literal 
-       String s1 = "Bharath Ravelli";
-
-       //Using String class
-       String s2 = new String("Tejaswi Ravelli");
-
-      //using character array
-      char[] arr= {'V','i','j','a','y','a'};
-      String s3 = new String(arr);
-
-      //Using a byte array 
-      String s4 = new String(new byte[]{65,66,67,68});
-      System.out.println(s1+"\n"+s2+"\n"+s3+"\n"+s4);
-      System.out.println();
-      System.out.println("Some important methods on Strings :");
-
-      System.out.println(
-        "int length():"+ s1.length()+"\n"+
-        "String toLowerCase():"+ s1.toLowerCase()+"\n"+
-        "String toUpperCase():"+s1.toUpperCase()+"\n"+
-        "String trim():"+"  spaces ".trim()+"\n"+
-        "String substring(int begin):"+ s1.substring(2)+"\n"+
-        "String substring(int begin,int end):"+s1.substring(2,5)+"\n"+
-        "String replace(char old):"+s1.replace('a','r')+"\n"+
-        "char charAt(int ind):"+s1.charAt(0)+"\n"+
-        "boolean startsWith(String s,int ind):"+s1.startsWith("R", 8)+"\n"+
-        "boolean endsWith(String s,int ind):"+s1.endsWith("lli")+"\n"+
-        "int indexOf(String s):"+s1.indexOf("Ravelli")+"\n"+
-        //"int lastIndexOf(String s)"
-        "boolean equals(String s):"+ s1.equals("Bharath Ravelli")+"\n"+
-        "boolean equalsIgnoreCase(String s):"+s1.equals("Bharathravelli")+"\n"+
-        "int compareTo(String s):"+s1.compareTo(s2)+"\n"+
-        "boolean contains(String str)"+s1.contains("Ravelli")       
-      );
+     void method4(){
+        System.out.println("Inside the Super class A");
     }
 }
-/*
-output:
+class B extends A{
+   protected void method4(){
+        
+    }
+}
 
-Bharath Ravelli
-Tejaswi Ravelli
-Vijaya
-ABCD
+public class Main{
+    public static void main(String[] args){
+           IDemo i = new IDemo(){ // we can actually override the abstract methods here and use the object
+                                 };
+           System.out.println(IDemo.meth1());
+           System.out.println(i.meth3());
+    }
+}
 
-Some important methods on Strings :
-int length():15
-String toLowerCase():bharath ravelli
-String toUpperCase():BHARATH RAVELLI
-String trim():spaces
-String substring(int begin):arath Ravelli
-String substring(int begin,int end):ara
-String replace(char old):Bhrrrth Rrvelli
-char charAt(int ind):B
-boolean startsWith(String s,int ind):true
-boolean endsWith(String s,int ind):true
-int indexOf(String s):8
-boolean equals(String s):true
-boolean equalsIgnoreCase(String s):false
-int compareTo(String s):-18
-boolean contains(String str)true
-
+/*outout
+-20
+2147483647
+Default method() called
  */
